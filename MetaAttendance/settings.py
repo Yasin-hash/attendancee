421adef6dd5257c76aa181c2872af5b5cb75c746
Yasin-hash <yasinarstanbekov78@gmail.com>
2024-09-29 13:53:05 +0600

    Update settings.py

--- MetaAttendance/settings.py
+++ MetaAttendance/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a1js_6%6y-x9-cqj5pv5)u$(b5m=-1eg2@5!7#d7qyw0-g#d@u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["web-production-9c0f.up.railway.app",]
 
 
 # Application definition
